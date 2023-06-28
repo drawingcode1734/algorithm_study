a56d96532614b65d759d07dd090848619f509f95
drawingcode1734 <lljy0213@naver.com>
2023-06-29 01:16:13 +0900

    pretty version

--- Practice/bj2562.java
+++ Practice/bj2562.java
@@ -5,25 +5,23 @@ public class bj2562 {
         Scanner sc = new Scanner(System.in);
 
         int[] array = new int[9]; // 길이가 9인 배열 생성
-        for(int i = 0; i < 9; i++) {
-            array[i] = sc.nextInt();
-        }
 
         int max = 0;
         int max_num = 0;
 
-        for(int i = 0; i < 8; i++) {
-            if(array[max_num] < array[i+1]) {
-                max = array[i+1];
-                max_num = i+2;
+        for(int i = 0; i < 9; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < 9; i++) {
+            if(array[i] > max) { // 바로 max값과 비교하는 것으로 수정함
+                max = array[i];
+                max_num = i+1; // index가 아니라 몇 번째에 있는지 구하라고 했으므로 +1 해줌
             }
-            max = array[max_num];
         }
 
         System.out.println(max);
         System.out.println(max_num);
 
-
     }
-
 }
