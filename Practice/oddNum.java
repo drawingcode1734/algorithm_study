68f047a5e23d07d6d62b220a9f4318e1423dc685
drawingcode1734 <lljy0213@naver.com>
2023-06-28 17:39:14 +0900

    oddNumber

---
+++ Practice/oddNum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class oddNum{
+
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 입력 : ");
+        int num = sc.nextInt();
+        
+        for(int i = 1 ; i <= num ; i++) {
+            for(int j = num-i ; j >= 0 ; j--)
+                System.out.print(" ");
+            
+            for(int k = 1 ; k <= i ; k++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+    }
+}
+
